d824d16e1889d4c18515f3c5b536de0eccee6941
Petteri Susi <susi@iki.fi>
2019-08-19 22:00:51 +0300

    Add anchor to settings

---
+++ Bookbeep/ConfigTableViewCell.swift
@@ -0,0 +1,53 @@
+//
+//  ConfigTableViewCell.swift
+//  Bookbeep
+//
+//  Created by Petteri Susi on 15/08/2019.
+//  Copyright © 2019 Petteri Susi. All rights reserved.
+//
+
+import UIKit
+
+class ConfigTableViewCell: UITableViewCell {
+    
+    let cellView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    let dayLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Configure"
+        label.textColor = UIColor.black
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        addSubview(cellView)
+        cellView.addSubview(dayLabel)
+        self.selectionStyle = .none
+        
+        NSLayoutConstraint.activate([
+            cellView.topAnchor.constraint(equalTo: self.topAnchor, constant: 0),
+            cellView.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -10),
+            cellView.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 10),
+            cellView.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+        ])
+        
+        dayLabel.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        dayLabel.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        dayLabel.centerYAnchor.constraint(equalTo: cellView.centerYAnchor).isActive = true
+        dayLabel.leftAnchor.constraint(equalTo: cellView.leftAnchor, constant: 20).isActive = true
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    
+}
--- Bookbeep/ViewController.swift
+++ Bookbeep/ViewController.swift
@@ -16,9 +16,77 @@ struct Bookdump {
     static let API_ROOT = "http://10.1.1.196:5000/api";
 }
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate,  UITableViewDataSource {
     @IBOutlet var pushScannerButton: UIButton!
     @IBOutlet weak var photoButton: UIButton!
+    
+    let tableview: UITableView = {
+        let tv = UITableView()
+        tv.backgroundColor = UIColor.white
+        tv.translatesAutoresizingMaskIntoConstraints = false
+        return tv
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableview.delegate = self
+        tableview.dataSource = self
+        tableview.register(ConfigTableViewCell.self, forCellReuseIdentifier: "cellId")
+        
+        view.addSubview(tableview)
+        
+        NSLayoutConstraint.activate([
+            tableview.heightAnchor.constraint(equalToConstant: 100),
+            tableview.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 100),
+            tableview.rightAnchor.constraint(equalTo: self.view.rightAnchor),
+            tableview.leftAnchor.constraint(equalTo: self.view.leftAnchor)
+        ])
+    }
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = UIView.init(frame: CGRect.init(x: 0, y: 0, width: tableView.frame.width, height: 50))
+        let label = UILabel()
+        label.frame = CGRect.init(x: 10, y: 25, width: headerView.frame.width-10, height: headerView.frame.height - 20)
+        label.text = "BOOKDUMP SERVER"
+        label.textColor = .black
+        label.font = label.font.withSize(12)
+        label.textColor = .darkGray
+        headerView.addSubview(label)
+        
+        let px = 1 / UIScreen.main.scale
+        let line = UIView(frame: CGRect(x: 0, y: headerView.frame.height, width: tableView.frame.width, height: px))
+        line.backgroundColor = tableView.separatorColor
+        headerView.addSubview(line)
+        NSLayoutConstraint.activate([line.widthAnchor.constraint(equalTo: headerView.widthAnchor),
+            headerView.widthAnchor.constraint(equalTo: tableView.widthAnchor)])
+
+        return headerView
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableview.dequeueReusableCell(withIdentifier: "cellId", for: indexPath) as! ConfigTableViewCell
+        cell.backgroundColor = UIColor.white
+        cell.separatorInset.left = 0
+        cell.accessoryType = .disclosureIndicator
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
     
     @IBAction func handleScannerPush(_ sender: Any, forEvent event: UIEvent) {
         let viewController = makeBarcodeScannerViewController()
